02430f8632bf501af029dcbf9a09119e58a9684c
Marius Küng <mk@mariuskueng.ch>
2012-07-19 20:21:39 +0200

    Added project files

---
+++ django_project/urls.py
@@ -0,0 +1,17 @@
+import os
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_project.views.home', name='home'),
+    # url(r'^django_project/', include('django_project.foo.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': os.path.join(os.path.dirname(os.path.dirname(__file__)), 'media/')}),
+)
